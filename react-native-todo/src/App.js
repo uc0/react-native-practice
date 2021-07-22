496206946007e69d1e0c598fb21979de03c30fd7
유찬영 <chan@thecommerce.co.kr>
2021-07-22 14:00:15 +0900

    todo 수정 기능 추가

--- react-native-todo/src/App.js
+++ react-native-todo/src/App.js
@@ -58,6 +58,12 @@ export default function App() {
         setTasks(currentTasks)
     }
 
+    const _updateTask = item => {
+        const currentTasks = Object.assign({}, tasks)
+        currentTasks[item.id] = item
+        setTasks(currentTasks)
+    }
+
     const _handleTextChange = text => {
         setNewTask(text)
     }
@@ -72,7 +78,7 @@ export default function App() {
                     {Object.values(tasks)
                         .reverse()
                         .map(item => (
-                            <Task key={item.id} item={item} deleteTask={_deleteTask} toggleTask={_toggleTask}/>
+                            <Task key={item.id} item={item} deleteTask={_deleteTask} toggleTask={_toggleTask} updateTask={_updateTask}/>
                         ))}
                 </List>
             </Container>
--- react-native-todo/src/components/Task.js
+++ react-native-todo/src/components/Task.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components/native'
 import PropTypes from 'prop-types'
 import IconButton from './IconButton'
 import { images } from '../images'
+import Input from './Input'
 
 const Container = styled.View`
     flex-direction: row;
@@ -20,12 +21,33 @@ const Contents = styled.Text`
     text-decoration-line: ${({ completed }) => completed ? 'line-through' : 'none'}
 `
 
-const Task = ({ item, deleteTask , toggleTask }) => {
-    return (
+const Task = ({ item, deleteTask , toggleTask, updateTask}) => {
+    const [isEditing, setIsEditing] = useState(false)
+    const [text, setText] = useState(item.text)
+
+    const _handleUpdateButtonPress = () => {
+        setIsEditing(true)
+    }
+
+    const _onSubmitEditing = () => {
+        if (isEditing) {
+            const editedTask = Object.assign({}, item, { text })
+            setIsEditing(false)
+            updateTask(editedTask)
+        }
+    }
+
+    return isEditing ? (
+        <Input 
+            value={text}
+            onChangeText={text => setText(text)}
+            onSubmitEditing={_onSubmitEditing}
+        />
+    ) : (
         <Container>
             <IconButton type={item.completed ? images.completed : images.uncompleted} id={item.id} onPressOut={toggleTask} completed={item.completed}/>
             <Contents completed={item.completed} >{item.text}</Contents>
-            {item.completed || <IconButton type={images.update} />}
+            {item.completed || <IconButton type={images.update} onPressOut={_handleUpdateButtonPress}/>}
             <IconButton type={images.delete} id={item.id} onPressOut={deleteTask} completed={item.completed}/>
         </Container>
     )
@@ -34,7 +56,8 @@ const Task = ({ item, deleteTask , toggleTask }) => {
 Task.propTypes = {
     item: PropTypes.object.isRequired,
     deleteTask: PropTypes.func.isRequired,
-    toggleTask: PropTypes.func.isRequired
+    toggleTask: PropTypes.func.isRequired,
+    updateTask: PropTypes.func.isRequired
 }
 
 export default Task
