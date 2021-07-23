45b328963b756291bec1a4629dbeae409cee2e68
유찬영 <uchan01001@gmail.com>
2021-07-23 11:23:46 +0900

    contextAPI 시작

---
+++ react-native-context/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+const Container = styled.View`
+    flex: 1;
+    background-color: #ffffff;
+    justify-content: center;
+    align-items: center;
+`
+
+const App = () => {
+    return <Container></Container>
+}
+
+export default App
